4cae44383fee3738674e348c5a4c5f7cd0d40935
RafaSMGam <153822342+RafaSMGam@users.noreply.github.com>
2023-12-27 15:48:37 -0300

    Add files via upload

--- i18n.js
+++ i18n.js
@@ -299,7 +299,7 @@ var i18n = {
 				pattern: `beber`
 			},
 			wind: {
-				pattern: `(carregar|ativar)`
+				pattern: `(ligar|ativar)`
 			},
 			kill: {
 				pattern: `(matar|atacar|golpear|ferir|eliminar|abater)`,
@@ -560,6 +560,7 @@ var i18n = {
 				pattern: ` (tra|fra) `,
 				replaceWith: ` entre `
 			},
+
 
 		]
 	},
