6e0f258bb70cde0f32ed700a74558215cffe6314
RafaSMGam <153822342+RafaSMGam@users.noreply.github.com>
2023-12-27 15:09:57 -0300

    Update i18n.js

--- i18n.js
+++ i18n.js
@@ -299,7 +299,7 @@ var i18n = {
 				pattern: `beber`
 			},
 			wind: {
-				pattern: `(dar corda  (?:em)`
+				pattern: `(dar( corda)? (?:em)`
 			},
 			kill: {
 				pattern: `(matar|atacar|golpear|ferir|eliminar|abater)`,
